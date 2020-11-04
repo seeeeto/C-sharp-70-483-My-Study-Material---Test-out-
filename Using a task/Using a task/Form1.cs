937d2711ac7d7a8923225b630604507095ac35dc
Shigeto <43973443+seeeeto@users.noreply.github.com>
2020-11-04 20:33:28 +0000

    Add files via upload

---
+++ Using a task/Using a task/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Using_a_task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double computeAverages(long noOfValues)
+        {
+            double total = 0;
+            Random rand = new Random();
+            for (long values = 0; values < noOfValues; values++)
+            {
+                total = total + rand.NextDouble();
+            }
+            return total / noOfValues;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long noOfValues = long.Parse(NumberOfValuesTextBox.Text);
+            Task.Run(() =>
+           {
+               ResultTextBoxBlock.Text = computeAverages(noOfValues).ToString();
+           });
+        }
+    }
+}
